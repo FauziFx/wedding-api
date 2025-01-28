8d1626861f1763bc4f28bd08d75121b931e95f1b
Ahmad Fauzi <fauzitkj3@gmail.com>
2025-01-28 14:13:07 +0700

    fix: add console.log in error handler

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -5,6 +5,8 @@ const ErrorHandlers = (err, req, res, next) => {
   console.log("Middleware Error Handling");
   const errStatus = err.statusCode || 500;
   const errMsg = err.message || "Something went wrong";
+  const log = process.env.NODE_ENV === "development" ? err : {};
+  console.log(log);
   res.status(errStatus).json({
     success: false,
     status: errStatus,
